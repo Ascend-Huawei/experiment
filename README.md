45d317953b19f71921a72659dc00411e99b26837
tanmana <tanmana.sadhu1@huawei.com>
2020-12-15 17:23:46 -0800

    adding experiment files

--- README.md
+++ README.md
@@ -1,1 +1,128 @@
-# experiment
+# Head Pose Estimation
+This model recognizes the head pose of a person in terms of 3 angles: *yaw*, *pitch* and *roll* in an image. 
+The face detection model is used to locate the face in the image prior to inferring the head pose from the detected face.
+
+## Model Description
+
+#### Models:
+Here we are using offline models for 1. face detection 2. head pose estimation to inference on the board. 
+
+
+
+1. Face Detection
+
+Download the weight and network files to your project directory 'head_pose':
+
+- Weights: https://obs-model-ascend.obs.cn-east-2.myhuaweicloud.com/face_detection/face_detection.caffemodel
+- Network: https://github.com/Ascend-Huawei/models/blob/master/computer_vision/object_detect/face_detection/face_detection.prototxt
+
+Execute the following command from your project directory 'head_pose' to convert the pre-trained model for face detection to offline model (.om) format:
+
+**atc --output_type=FP32 --input_shape="data:1,3,300,300" --weight="face_detection.caffemodel" --input_format=NCHW --output="face_detection" --soc_version=Ascend310 
+--insert_op_conf=insert_op.cfg --framework=0 --save_original_model=false 
+--model="face_detection.prototxt"**
+
+
+
+2. Head Pose Estimation
+
+Download the weights and network files to your project directory 'head_pose':
+
+- Weights:
+https://obs-model-ascend.obs.cn-east-2.myhuaweicloud.com/head_pose_estimation/head_pose_estimation.caffemodel
+- Network: https://github.com/Ascend-Huawei/models/blob/master/computer_vision/object_detect/head_pose_estimation/head_pose_estimation.prototxt
+
+
+Execute the following command from your project directory 'head_pose' to convert the pre-trained model for head pose estimation to offline model (.om) format:
+
+**atc --output_type=FP32 --input_shape="data:1,3,224,224" --weight="head_pose_estimation.caffemodel" --input_format=NCHW --output="head_pose_estimation"
+--soc_version=Ascend310 --framework=0 --save_original_model=false --model="head_pose_estimation.prototxt"**
+
+
+#### Inputs
+The input for face detection model are as follows:
+- **Input Shape**: [1,300,300, 3]
+- **Input Format** : NCHW
+- **Input Type**: FLOAT32
+
+The input for the head pose estimation model are as follows:
+- **Input Shape**: [1,3, 224, 224]
+- **Input Format** : NCHW
+- **Input Type**: FLOAT32
+
+#### Outputs
+The outputs for the face detection model are as follows:
+- The pre-trained model will detect 2 types: face and others.
+- Output is a list of shape: (1, 8)
+  - **0 position**: not used
+  - **1 position**: label
+  - **2 position**: confidence score
+  - **3 position**: top left x coordinate
+  - **4 position**: top left y coordinate
+  - **5 position**: bottom right x coordinate
+  - **6 position**: bottom right y coordinate
+  - **7 position**: not used
+
+The outputs for the head pose estimation model are as follows:
+- List of numpy arrays: 
+  - **List shapes**: (1, 136, 1, 1), (1, 3, 1, 1)
+The first list is a set of 136 facial keypoints. The second list in the output containing the 3 values of yaw, pitch, roll angles predicted by the model, which are used to determine head pose based on some preset rules.
+
+Output printed to terminal (sample):
+```
+Head angles: [array([[9.411621]], dtype=float32), array([[7.91626]], dtype=float32), array([[-1.0116577]], dtype=float32)]
+Pose: Head Good posture
+```
+Result image with 64 keypoints plotted on detected face saved in 'out' folder.
+
+  
+## Code:
+
+  - All code files needed to run the experiment are included in folder 'head-pose'. The script 'head_pose_estimation.py' contains all the preprocessing, model inference and post_processing methods. 
+  
+  **Please Note:** You would need to complete all the code as per instructions, before running the application.
+  
+  - Preprocessing: 
+    - **Resize**: (224, 224)
+    - **Image Type**: FLOAT32
+    - **Input Format** : NCHW
+    - Change order from **[300, 300, 3]**(HWC) to **[3, 300, 300]**(CHW)
+  
+  - The om model file (.om) must be downloaded to the project folder 'head_pose'
+ 
+  - Postprocessing:
+    - Infer head pose from yaw, pitch and roll angles, using fixed range thresholds.
+
+    
+  - To run code, simply using commands below in the terminal:
+  
+    ``` 
+    cd experiments/head_pose
+    python3 head_pose_estimation.py 
+    ``` 
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ head_pose/head_pose_estimation.py
@@ -0,0 +1,181 @@
+import numpy as np
+import cv2
+import sys
+sys.path.append('../')
+#ACL model load and execute implementation
+from acl_model import Model
+#ACL init and resource management implementation
+from acl_resource import AclResource 
+
+# Path for head pose model
+model_name_head_pose = 'head_pose_estimation'
+# Path for face detection model
+model_name_face_det = 'face_detection'
+# Test image
+img_file = 'face1.jpeg'
+
+def Main():
+
+	# Step 1: initialize ACL and ACL runtime 
+	# 1.1: one line of code to create an object of the 'AclResource' class
+	### Your code here, one line ###
+
+	# 1.2: one line of code, call the 'init' function of the AclResource object, to initilize ACL and ACL runtime 
+	### Your code here, one line ###
+
+	# Step 2: Load models for face detection and head pose estimation
+	# 2.1 load offline model for face detection
+	# Path for face detection model
+	MODEL_PATH = model_name_face_det + ".om" 
+	# one line of code to create an object of the 'Model' class with two parameters: the created 'AclResource' object and the 'MODEL_PATH'
+	### Your code here, one line ###
+
+
+	# 2.2 load offline model for head pose estimation
+	# Path for head pose estimation model
+	MODEL_PATH = model_name_head_pose + ".om" 
+	# one line of code to create an object of the 'Model' class with two parameters: the created 'AclResource' object and the 'MODEL_PATH'
+	### Your code here, one line ###
+
+	# Step 3: Face Detection Inference
+	# load testing image file 
+	image = cv2.imread(img_file)
+	# preprocessing the image file for face detection
+	input_image = PreProcessing_face(image)
+
+	# one line of code, use the 'Model' object for face detection, and call its 'execute' function with parameter '[input_image]'
+	### Your code here, one line ###
+
+	# Postprocessing to get the bounding box
+	try:
+		xmin, ymin, xmax, ymax = PostProcessing_face(image, resultList_face)
+		bbox_list = [xmin, ymin, xmax, ymax]
+	except:
+		print('\n***Error***: Face detection inference result is not ready for post processing\n')
+		return
+		
+	# Step 4: Head Pose Estimation
+	# Preprocessing for head pose estimation
+	input_image = PreProcessing_head(image, bbox_list)
+	# head pose estimation model inference
+	# one line of code, use the 'Model' object for head pose detection, and call its 'execute' function with parameter '[input_image]'
+	### Your code here, one line ###
+
+	#post processing to obtain coordinates for lines drawing
+	try:
+		facepointList, head_status_string = PostProcessing_head(resultList_head, bbox_list, image)
+	except:
+		print('\n***Error***: Head Pose estimation inference result is not ready for post processing\n')
+		return
+	print('Head angles:', resultList_head[2])
+	print('Pose:', head_status_string)
+
+# face detection model preprocessing
+def PreProcessing_face(image):
+	image = cv2.resize(image, (300,300))
+	image = image.astype('float32')
+	image = np.transpose(image, (2, 0, 1)).copy()
+	return image
+
+# face detection model post processing
+def PostProcessing_face(image, resultList, threshold=0.9):
+	detections = resultList[1]
+	bbox_num = 0
+	bbox_list = []
+	for i in range(detections.shape[1]):
+		det_conf = detections[0,i,2]
+		det_xmin = detections[0,i,3]
+		det_ymin = detections[0,i,4]
+		det_xmax = detections[0,i,5]
+		det_ymax = detections[0,i,6]
+		bbox_width = det_xmax - det_xmin
+		bbox_height = det_ymax - det_ymin
+		if threshold <= det_conf and 1>=det_conf and bbox_width>0 and bbox_height > 0:
+			bbox_num += 1
+			xmin = int(round(det_xmin * image.shape[1]))
+			ymin = int(round(det_ymin * image.shape[0]))
+			xmax = int(round(det_xmax * image.shape[1]))
+			ymax = int(round(det_ymax * image.shape[0]))
+			print('BBOX:', xmin, ymin, xmax, ymax)
+			cv2.rectangle(image,(xmin,ymin),(xmax,ymax),(0,255,0),1)
+		else:
+			continue
+	print("detected bbox num:", bbox_num)
+	return [xmin, ymin, xmax, ymax]
+
+
+# head pose estimation preprocessing
+def PreProcessing_head(image, boxList):
+	# convert to float type
+	image = np.asarray(image, dtype=np.float32)
+	# crop out detected face
+	image = image[int(boxList[1]):int(boxList[3]),int(boxList[0]):int(boxList[2])]
+	# resize to required input dimensions
+	image = cv2.resize(image, (224, 224))
+	# switch from NHWC to NCHW format
+	image = np.transpose(image, (2, 0, 1)).copy()
+	return image
+	
+
+'''Complete the following function to find the viewing direction 
+from predicted head pose angles (pitch, yaw, roll)
+
+HINT: The first, second and third element of the input list 'resultList' 
+correspond to pitch, yaw and roll respectively.'''
+def head_status_get(resultList):
+	# initialize
+	fg_pitch = True
+	fg_yaw = True
+	fg_roll = True
+	head_pose = ''
+	
+	# Assign 'up' or 'down' values to 'head_pose' from pitch angle values. 
+	if resultList[2][0] < -20:
+		head_pose = 'up'
+	elif resultList[2][0] > 20:
+		head_pose = 'down'
+	else:
+		fg_pitch = False
+
+	# Assign 'left' or 'right' values to 'head_pose' from yaw angle values. 
+	### Your code here ###
+	
+	# Assign 'swing left' or 'swing right' values to 'head_pose' from roll angle values. 
+	### Your code here ###
+
+	if fg_pitch is False and fg_yaw is False and fg_roll is False:
+		head_pose = ' Good posture'
+	return head_pose
+
+def PostProcessing_head(resultList, boxList, image):
+	resultList.append([resultList[1][0][0] * 50, resultList[1][0][1] * 50, resultList[1][0][2] * 50])
+	HeadPosePoint = []
+	facepointList = []
+	box_width = boxList[2] - boxList[0] 
+	box_height = boxList[3] - boxList[1]
+	box_width = box_width
+	box_height = box_height
+	print('box width:', box_width)
+	print('box height:', box_height)
+	for j in range(136):
+		if j % 2 == 0:
+			HeadPosePoint.append((1+resultList[0][0][j])/2  * box_width + boxList[0])
+		else:
+			HeadPosePoint.append((1+resultList[0][0][j])/2  * box_height + boxList[1])
+		facepointList.append(HeadPosePoint)
+	for j in range(136):
+		if j % 2 == 0:
+			canvas = cv2.circle(image, (int(facepointList[0][j]), int(facepointList[0][j+1])), 
+								radius=5, color=(255, 0, 0), thickness=2)
+	head_status_string = head_status_get(resultList)
+	cv2.putText(canvas,head_status_string,(10,10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,0),1)
+	cv2.imwrite('out/result_out.jpg', canvas)
+	return facepointList, head_status_string
+
+
+if __name__ == '__main__':
+	Main()
+			
+
+
+
